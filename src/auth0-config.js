7b113df39852cf7b240fef61e51614ad3509ae99
Sam-Checkers <samcollins713@gmail.com>
2024-03-24 22:18:41 -0400

    first commit

--- src/auth0-config.js
+++ src/auth0-config.js
@@ -3,7 +3,7 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const Auth0ProviderConfig = {
   domain: 'dev-44w2oefzt0a2lc21.us.auth0.com',
   clientId: 'znxWjP8amyuhWvZO3P3PpIkUgWHjuEEc',
-  redirectUri: 'https://ephemeral-kheer-f078b9.netlify.app'
+  redirectUri: 'https://ephemeral-kheer-f078b9.netlify.app/'
 };
 
 export default Auth0ProviderConfig;
